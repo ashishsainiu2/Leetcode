330ad31279c899e42bcf724b64937e6bb8421606
ASHISH KUMAR SAINI <40246874+ashishsainiu2@users.noreply.github.com>
2024-04-16 12:27:15 +0530

    Time: 595 ms (23.18%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-with cte as(
-select * , case when id%2 !=0 then 1 else 0 end as flag from cinema)
-select id, movie, description, rating from cte where flag = 1 and description <> 'boring' order by rating desc
+
+select *  from cinema where id%2 <> 0 and description <> 'boring' order by rating desc
